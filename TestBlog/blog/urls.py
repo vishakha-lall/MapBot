083468fb4c02a303005f053e310f7b98247a48b1
vishakha-lall <vishakha.lall@outlook.com>
2017-10-27 21:34:06 +0530

    Added individual blog views

--- TestBlog/blog/urls.py
+++ TestBlog/blog/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from . import views
+
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
--- TestBlog/blog/views.py
+++ TestBlog/blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import render, get_object_or_404
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
