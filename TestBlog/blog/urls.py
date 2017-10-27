93693fe5ce54d0fad9b3d92ae927e369af8ba4dd
vishakha-lall <vishakha.lall@outlook.com>
2017-10-27 21:17:24 +0530

    Test Blog initial commit

---
+++ TestBlog/blog/urls.py
@@ -0,0 +1,5 @@
+from django.conf.urls import url
+from . import views
+urlpatterns = [
+    url(r'^$', views.post_list, name='post_list'),
+]
---
+++ TestBlog/blog/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
