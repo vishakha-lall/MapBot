a2a3f8e9109bacdda11330b0e89b2c5f985eb2aa
Ankur Chattopadhyay <chttrjeankr@gmail.com>
2020-03-02 15:55:16 +0530

    Removed config.py from reflecting changes

--- config.py
+++
@@ -1,9 +0,0 @@
-user = "root"
-password = "root"
-host = "localhost"
-database = "mapbot"
-port = "3306"
-#key = "*Google_Cloud_API_key*" #Will be provided by mentors
-#stanford_path_to_jar = "*your_path_to_stanford-corenlp-x.x.x.jar*" #for eg. "stanford-corenlp-full-2018-10-05/stanford-corenlp-3.9.2.jar"
-#stanford_path_to_models_jar = "*your_path_to_stanford-corenlp-x.x.x-models.jar*" #for eg. "stanford-corenlp-full-2018-10-05/stanford-corenlp-3.9.2-models.jar"
-#javahome = '*your_path_to_jdk_bin_java.exe*' #for eg. "C:/Program\ Files/Java/jdk1.8.0_201/bin/java.exe"
