8edd4afb128b0ad90809679c0d1b93a590efe60f
hritikpapa <kvnl.hritik@gmail.com>
2020-03-05 19:08:00 +0530

    Used Path function imported from pathlib for uniform path referencing that is compatible with all OS

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
+from pathlib import Path
 user = "root"
 password = "root"
 host = "localhost"
 database = "mapbot"
 port = "3306"
 key = "*Google_Cloud_API_key*" #Will be provided by mentors
-stanford_path_to_jar = "./stanford-corenlp-full-2018-10-05/stanford-corenlp-3.9.2.jar" #"*your_path_to_stanford-corenlp-x.x.x.jar*"
-stanford_path_to_models_jar = "./stanford-corenlp-full-2018-10-05/stanford-corenlp-3.9.2-models.jar" #"*your_path_to_stanford-corenlp-x.x.x-models.jar*"
-javahome = '*your_path_to_jdk_bin_java.exe*'#for eg. 'C:\\Program\ Files\\Java\\jdk1.8.0_201\\bin\\java.exe' or '/usr/local/openjdk-11/bin/java'
+stanford_path_to_jar = Path("./stanford-corenlp-full-2018-10-05/stanford-corenlp-3.9.2.jar") #"*your_path_to_stanford-corenlp-x.x.x.jar*"
+stanford_path_to_models_jar = Path("./stanford-corenlp-full-2018-10-05/stanford-corenlp-3.9.2-models.jar") #"*your_path_to_stanford-corenlp-x.x.x-models.jar*"
+javahome = Path('*your_path_to_jdk_bin_java.exe*')#for eg. 'C:\\Program\ Files\\Java\\jdk1.8.0_201\\bin\\java.exe' or '/usr/local/openjdk-11/bin/java'
 
 # DONOT CHANGE THE VALUES BELOW DURING INITIAL CONFIGURATION SET UP
 import os
@@ -15,4 +16,4 @@ if os.getenv("DOCKER"):
     user = "root"
     password = "root"
     host = "db"
-    javahome = '/usr/local/openjdk-11/bin/java'
+    javahome = Path('/usr/local/openjdk-11/bin/java')
