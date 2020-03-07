9888cb1ecaf0578ad0295244a47bbd4be995fa07
hritikpapa <kvnl.hritik@gmail.com>
2020-03-07 17:53:25 +0530

    Added TimeZoneAPI

--- googleMapsApiModule.py
+++ googleMapsApiModule.py
@@ -16,6 +16,12 @@ def direction(origin,destination):
     print(result_url)
     webbrowser.open_new(result_url)
 
+def timezone(search_location):
+    address = search_location.lower()
+    address = address.replace(" ","+")
+    result_url = 'https://maps.googleapis.com/maps/api/timezone/json?location=3'+address+'9.6034810,-119.6822510&timestamp=1331766000&key='+config.key
+    print(result_url)
+    webbrowser.open_new(result_url)
 
 def geocoding(search_location):
     gmaps = googlemaps.Client(config.key)
@@ -33,3 +39,4 @@ def mapsstatic(search_location):
     result_url = "https://maps.googleapis.com/maps/api/staticmap?center="+address+"&zoom=13&scale=1&size=600x350&maptype=roadmap&key="+config.key+"&format=png&visual_refresh=true&markers=size:mid%7Ccolor:0xff0000%7Clabel:L%7C"+address
     print(result_url)
     webbrowser.open_new(result_url)
+
