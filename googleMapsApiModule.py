91aea51dad37c01c9de614dbd0ea7d3172e15f88
adityabisoi <adityabisoi1999@gmail.com>
2020-03-09 22:38:24 +0530

    elevation API added

--- googleMapsApiModule.py
+++ googleMapsApiModule.py
@@ -2,6 +2,7 @@ import googlemaps
 import webbrowser
 import config
 import mysql.connector
+import requests
 from constants import BASE_URL
 
 gmaps = googlemaps.Client(config.key)      #global variable gmaps
@@ -12,7 +13,6 @@ def direction(origin,destination):
     result_url = f'{BASE_URL["direction"]}&{address.lower().replace(" ", "+")}'
     print(result_url)
     webbrowser.open_new(result_url)
-
 
 def geocoding(search_location):
     result = gmaps.geocode(search_location)
@@ -27,3 +27,11 @@ def mapsstatic(search_location):
     result_url = f'https://maps.googleapis.com/maps/api/staticmap?center={address.lower().replace(" ", "+")}&zoom=13&scale=1&size=600x350&maptype=roadmap&key={config.key}&format=png&visual_refresh=true&markers=size:mid%7Ccolor:0xff0000%7Clabel:L%7C{address.lower().replace(" ", "+")}'
     print(result_url)
     webbrowser.open_new(result_url)
+
+def elevation(search_location):
+    result = gmaps.geocode(search_location)
+    json = requests.get(f'https://maps.googleapis.com/maps/api/elevation/json?locations={result[0]["geometry"]["location"]["lat"]},{result[0]["geometry"]["location"]["lng"]}&key={config.key}').json()
+    print(json['results'][0]['elevation'])
+
+
+
