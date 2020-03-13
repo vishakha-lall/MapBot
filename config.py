1a87f8c9e5c803e3336d0794accf6cf545e54bd4
Mansi145 <guptamansi9350@gmail.com>
2020-03-13 14:27:20 +0530

    fixes

--- config.py
+++ config.py
@@ -1,8 +1,19 @@
+import os
 user = "root"
-password = "localhost"
+password = "root"
 host = "localhost"
 database = "mapbot"
-#key = "*Google_Cloud_API_key*" #Will be provided by mentors
-#stanford_path_to_jar = "*your_path_to_stanford-corenlp-x.x.x.jar*" #for eg. "stanford-corenlp-full-2018-10-05/stanford-corenlp-3.9.2.jar"
-#stanford_path_to_models_jar = "*your_path_to_stanford-corenlp-x.x.x-models.jar*" #for eg. "stanford-corenlp-full-2018-10-05/stanford-corenlp-3.9.2-models.jar"
-#javahome = '*your_path_to_jdk_bin_java.exe*' #for eg. "C:/Program\ Files/Java/jdk1.8.0_201/bin/java.exe"
+port = "3306"
+key = "*Google_Cloud_API_key*"  # Will be provided by mentors
+stanford_path_to_jar = "./stanford-corenlp-full-2018-10-05/stanford-corenlp-3.9.2.jar"  # your_path_to_stanford-corenlp-x.x.x.jar
+stanford_path_to_models_jar = "./stanford-corenlp-full-2018-10-05/stanford-corenlp-3.9.2-models.jar"  # your_path_to_stanford-corenlp-x.x.x-models.jar
+javahome = '*your_path_to_jdk_bin_java.exe*'  # for eg. 'C:\\Program\ Files\\Java\\jdk1.8.0_201\\bin\\java.exe' or '/usr/local/openjdk-11/bin/java'
+
+# DONOT CHANGE THE VALUES BELOW DURING INITIAL CONFIGURATION SET UP
+
+if os.getenv("DOCKER"):
+    # print("Inside Docker")
+    user = "root"
+    password = "root"
+    host = "db"
+    javahome = '/usr/local/openjdk-11/bin/java'
