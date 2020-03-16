42b2dc8c50f57c5a542ed10ce6484a2b410d2a29
asthacs <astha.jain99@gmail.com>
2020-03-16 23:13:25 +0530

    Implemented clean exit for conversation

--- init.py
+++ init.py
@@ -1,7 +1,9 @@
 from chatbot import setup
 from chatbot import message_to_bot
-clf, learn_response = setup()
+clf, learn_response = setup()    
 while(True):
-    received_message = input("You: ")
-    send_message, learn_response = message_to_bot(received_message, clf, learn_response)
-    print("MapBot:  " + send_message)
+	received_message = input("You: ")
+	send_message, learn_response = message_to_bot(received_message,clf,learn_response)
+	print("MapBot: "+send_message)
+	if received_message.lower() == "bye" or received_message.lower() == "bye." or received_message.lower() == "bye!":															#exit loop
+		break
