f36bb0e7427eb8497df23390b427e1f1fd3254a1
asthacs <astha.jain99@gmail.com>
2020-03-18 19:01:25 +0530

    Fixed the representation for constants

--- init.py
+++ init.py
@@ -1,10 +1,10 @@
 from chatbot import setup
 from chatbot import message_to_bot
 clf, learn_response = setup()
-exit_conversation = "Bye! I'll miss you!"
+EXIT_CONVERSATION = "Bye! I'll miss you!"
 while(True):
     received_message = input("You: ")
     send_message, learn_response = message_to_bot(received_message, clf, learn_response)
     print("MapBot:  " + send_message)
-    if send_message == exit_conversation:
+    if send_message == EXIT_CONVERSATION:
     	break
