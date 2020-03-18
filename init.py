3408a8e4dd97e65874273b6f79826244567b6522
asthacs <astha.jain99@gmail.com>
2020-03-18 15:52:19 +0530

    Improved code to handle exit of conversation

--- init.py
+++ init.py
@@ -1,9 +1,10 @@
 from chatbot import setup
 from chatbot import message_to_bot
 clf, learn_response = setup()
+exit_conversation = "Bye! I'll miss you!"
 while(True):
     received_message = input("You: ")
     send_message, learn_response = message_to_bot(received_message, clf, learn_response)
     print("MapBot:  " + send_message)
-	if received_message.lower() == "bye" or received_message.lower() == "bye." or received_message.lower() == "bye!":															#exit loop
-		break
+    if send_message == exit_conversation:
+    	break
