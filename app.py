a29d92299eb7fb86a878f74b5a176a950fa12777
rits1272 <ritikjain1272@gmail.com>
2020-03-20 12:49:26 +0530

    Added endpoint for chatbot

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+from chatbot import message_to_bot, setup
+
+app = Flask(__name__)
+
+@app.route('/chatbot/<input_user>', methods=['GET'])
+def chat(input_user):
+    clf, learn_response = setup()
+    try:
+        response = message_to_bot(input_user, clf, learn_response)
+    except:
+        return jsonify({'error' : "Unable to get response"})
+    
+    return jsonify({'chatbot' : response})
+
+if __name__ == '__main__':
+    app.run(debug=False)
--- utilities.py
+++ utilities.py
@@ -1,5 +1,12 @@
 def setup_nltk():
     import nltk
+    import ssl
+    try:
+        _create_unverified_https_context = ssl._create_unverified_context
+    except AttributeError:
+        pass
+    else:
+        ssl._create_default_https_context = _create_unverified_https_context
     nltk.download('punkt')
     nltk.download('averaged_perceptron_tagger')
     nltk.download('stopwords')
