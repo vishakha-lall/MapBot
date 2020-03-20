9f71b15f5166709144df0bee8c6751d20a4a0c1b
Ritik Jain <ritikjain1272@gmail.com>
2020-03-20 23:41:31 +0530

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-user = "root"
-password = "Ritikjain@1272"
-host = "localhost"
-database = "mapbot"
-#key = "*Google_Cloud_API_key*" #Will be provided by mentors
-stanford_path_to_jar = "../stanford-corenlp-full-2018-10-05/stanford-corenlp-3.9.2.jar"
-stanford_path_to_models_jar = "../stanford-corenlp-full-2018-10-05/stanford-corenlp-3.9.2-models.jar"
-javahome = "/usr/lib/jvm/java-8-openjdk-amd64"
