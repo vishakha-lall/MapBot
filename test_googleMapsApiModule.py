35e5d6cc2b7417796f3da994b7cf43ed3e62e7a1
adityabisoi <adityabisoi1999@gmail.com>
2020-03-21 18:48:49 +0530

    exception handlers added

--- test_googleMapsApiModule.py
+++ test_googleMapsApiModule.py
@@ -1,27 +1,36 @@
 import googleMapsApiModule
 import config
 import pytest
-
-@pytest.mark.parametrize('source,destination',
-                                            [
-                                                ('paris','brussels'),    ## Add places for testing here
-                                                ('denver','boston'),
-                                                ('efef','weufhw')
-                                            ])
+from googlemaps.exceptions import ApiError
+ 
 class TestClass:
-    def test_direction(self,source,destination):
-        result = googleMapsApiModule.direction(source,destination)
-        assert result == "https://www.google.com/maps/dir/?api=1&origin="+source+"&destination="+destination
+    def test_direction(self):
+        result = googleMapsApiModule.direction('paris','brussels')
+        assert result == "https://www.google.com/maps/dir/?api=1&origin=paris&destination=brussels"
 
-    def test_geocoding(self,source,destination):
-        result = googleMapsApiModule.geocoding(source)
-        assert result == "https://www.google.com/maps/search/?api=1&query="+source
+    def test_invalid_direction(self):
+        with pytest.raises(ApiError):
+            result = googleMapsApiModule.direction('kjajw','qwiuq')
+            assert result == "https://www.google.com/maps/dir/?api=1&origin=kjajw&destination=qwiuq"
 
-    def test_mapsstatic(self,source,destination):
-        result = googleMapsApiModule.mapsstatic(source)
-        assert result == "https://maps.googleapis.com/maps/api/staticmap?center="+source+"&zoom=13&scale=1&size=600x350&maptype=roadmap&key="+config.key+"&format=png&visual_refresh=true&markers=size:mid%7Ccolor:0xff0000%7Clabel:L%7C"+source
+    def test_geocoding(self):
+        result = googleMapsApiModule.geocoding('denver')
+        assert result == "https://www.google.com/maps/search/?api=1&query=denver"
 
-    def test_elevation(self,source,destination):
-        result = googleMapsApiModule.elevation(source)
+    def test_Invalid_geocoding(self):
+        with pytest.raises(IndexError):
+            result = googleMapsApiModule.geocoding('kahakd...')
+            assert result == "https://www.google.com/maps/search/?api=1&query=kahakd..."
+
+    def test_mapsstatic(self):
+        result = googleMapsApiModule.mapsstatic('sydney')
+        assert result == "https://maps.googleapis.com/maps/api/staticmap?center=sydney&zoom=13&scale=1&size=600x350&maptype=roadmap&key="+config.key+"&format=png&visual_refresh=true&markers=size:mid%7Ccolor:0xff0000%7Clabel:L%7Csydney"
+
+    def test_elevation(self):
+        result = googleMapsApiModule.elevation('moscow')
         assert type(result) is float
 
+    def test_invalid_elevation(self):
+        with pytest.raises(IndexError):
+            result = googleMapsApiModule.elevation('hihih')
+            assert type(result) is float
