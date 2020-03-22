3fc81a3d24443e569afe83e45eca94ac1325b25e
rits1272 <ritikjain1272@gmail.com>
2020-03-22 21:46:44 +0530

    Integrated Mapbot with slack

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ app = Flask(__name__)
 
 @app.route('/chatbot/<user_input>', methods=['GET'])
 def chat(user_input):
-    clf, learn_response = setup()
     try:
         response = message_to_bot(user_input, clf, learn_response)
     except:
-        return jsonify({'message': "Unable to get response"}, 500)
+        return jsonify({'message': ('Unable to get response', learn_response)}, 500)
     
     return jsonify({'message': response}, 200)
 
 if __name__ == '__main__':
+    clf, learn_response = setup()
     app.run(debug=False)
--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 import os
 user = "root"
-password = "root"
+password = "Ritikjain@1272"
 host = "localhost"
 database = "mapbot"
 port = "3306"
-key = "*Google_Cloud_API_key*"  # Will be provided by mentors
+key = "AIzaSyBuHPXeUJKVTiHwjY8m9bw4Rvv1fYOCxmk"  # Will be provided by mentors
 
 # your_path_to_stanford-corenlp-x.x.x.jar
-stanford_path_to_jar = "./stanford-corenlp-full-2018-10-05/stanford-corenlp-3.9.2.jar"
+stanford_path_to_jar = "../stanford-corenlp-full-2018-10-05/stanford-corenlp-3.9.2.jar"
 
 # your_path_to_stanford-corenlp-x.x.x-models.jar
-stanford_path_to_models_jar = "./stanford-corenlp-full-2018-10-05/stanford-corenlp-3.9.2-models.jar"
+stanford_path_to_models_jar = "../stanford-corenlp-full-2018-10-05/stanford-corenlp-3.9.2-models.jar"
 
 # for eg. 'C:\\Program\ Files\\Java\\jdk1.8.0_201\\bin\\java.exe' or '/usr/local/openjdk-11/bin/java'
 javahome = '*your_path_to_jdk_bin_java.exe*'
