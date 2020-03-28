42f1536c19a9b23bcdb60759b6d16e861435898e
Ankur Chattopadhyay <chttrjeankr@gmail.com>
2020-03-28 21:22:33 +0530

    git clone included in start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
+if [ ! -d MapBot ]; then
+    git clone https://github.com/vishakha-lall/MapBot.git -b gssoc-master
+    cd MapBot
+else
+    cd MapBot
+    git pull
+fi
+
+export GCLOUD_API_KEY=<YOUR_API_KEY_HERE>
+
 docker-compose up -d && docker attach mapbot_bot
