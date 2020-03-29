2da3f46ed01e6bd8ec42524e953fd0fff327c22c
RINKI NAG <rinkinag24@gmail.com>
2020-03-29 22:25:48 +0530

    update with function classify_model_adv

--- utilities.py
+++ utilities.py
@@ -53,6 +53,48 @@ def classify_model():
     predout = pd.DataFrame({'id': test['id'], 'predicted': preds, 'actual': test['class']})
     return clf
 
+@logger_config.logger
+# classification into statements questions and chat with more choice of ml algorithms parameter tuned by Gridsearchcv.
+# By default this uses random forest if no argument passed.by @eaglewarrior
+def classify_model_adv(model="rf"):
+    import numpy as np
+    import pandas as pd
+	from sklearn.ensemble import RandomForestClassifier
+    from sklearn.svm import SVC
+	from xgboost import XGBClassifier
+	from sklearn.ensemble import AdaBoostClassifier
+	from sklearn.tree import DecisionTreeClassifier
+	from sklearn.naive_bayes import MultinomialNB
+    FNAME = Path('analysis/featuresDump.csv')
+    df = pd.read_csv(filepath_or_buffer=FNAME, )
+    df.columns = df.columns[:].str.strip()                                      # Strip any leading spaces from col names
+    df['class'] = df['class'].map(lambda x: x.strip())
+    width = df.shape[1]
+    # split into test and training (is_train: True / False col)
+    np.random.seed(seed=1)
+    df['is_train'] = np.random.uniform(0, 1, len(df)) <= .75
+    train, test = df[df['is_train'] == True], df[df['is_train'] == False]
+    features = df.columns[1:width-1]  # remove the first ID col and last col=classifier
+    # Fit an  Model for "class" given features rf:random forest,xgb:xgboost,nb:naive bayes,ada:adaboost
+	if model=="svm":
+		clf = SVC(C=1000, cache_size=200, class_weight=None, coef0=0.0,\
+		decision_function_shape='ovr', degree=3, gamma=0.0001, kernel='rbf',\
+		max_iter=-1, probability=False, random_state=None, shrinking=True,\
+		tol=0.001, verbose=False)
+	elif model=="rf":
+		clf= RandomForestClassifier(n_jobs=2, n_estimators=100)
+	elif model=="xgb":
+		clf= XGBClassifier(learning_rate= 0.01, n_estimators= 500)
+	elif model=="nb":
+		clf= MultinomialNB()
+	## best performance by adaboost
+	elif model="ada":
+		clf=AdaBoostClassifier(learning_rate=0.001,n_estimators=2000)
+    clf.fit(train[features], train['class'])
+    # Predict against test set
+    preds = clf.predict(test[features])
+    predout = pd.DataFrame({'id': test['id'], 'predicted': preds, 'actual': test['class']})
+    return clf
 
 @logger_config.logger
 def classify_sentence(clf, user_input):
