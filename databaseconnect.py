1cd198e8248e62235a8d92b3d41ed4c741fa30d7
Nandini9634 <nandiniagarwal9634@gmail.com>
2020-03-30 23:20:08 +0530

    linting with flake8

--- databaseconnect.py
+++ databaseconnect.py
@@ -1,6 +1,6 @@
 import logging
 import logger_config
-from chatbot import LearnResponse
+import chatbot
 
 log = logging.getLogger(__name__)
 log.info('Entered module: %s' % __name__)
@@ -120,10 +120,10 @@ def get_question_response(subject, root, verb):
             cur.execute(f"SELECT sentence FROM statement_table WHERE verb='{verb}'")
             res = cur.fetchone()
             B = res[0]
-            return B, LearnResponse.MESSAGE.name
+            return B, chatbot.LearnResponse.MESSAGE.name
         else:
             B = "Sorry I don't know the response to this. Please train me."
-            return B, LearnResponse.TRAIN_ME.name
+            return B, chatbot.LearnResponse.TRAIN_ME.name
     else:
         cur.execute("SELECT subject FROM statement_table")
         res = cur.fetchall()
@@ -140,19 +140,19 @@ def get_question_response(subject, root, verb):
                 cur.execute(f"SELECT sentence FROM statement_table WHERE subject='{subject}'")
                 res = cur.fetchone()
                 B = res[0]
-                return B, LearnResponse.MESSAGE.name
+                return B, chatbot.LearnResponse.MESSAGE.name
             else:
                 if checkVerb[2:-2] == verb[0]:
                     cur.execute(f"SELECT sentence FROM statement_table WHERE subject='{subject}'")
                     res = cur.fetchone()
                     B = res[0]
-                    return B, LearnResponse.MESSAGE.name
+                    return B, chatbot.LearnResponse.MESSAGE.name
                 else:
                     B = "Sorry I don't know the response to this. Please train me."
-                    return B, LearnResponse.TRAIN_ME.name
+                    return B, chatbot.LearnResponse.TRAIN_ME.name
         else:
             B = "Sorry I don't know the response to this. Please train me."
-            return B, LearnResponse.TRAIN_ME.name
+            return B, chatbot.LearnResponse.TRAIN_ME.name
 
 
 @logger_config.logger
@@ -173,7 +173,7 @@ def learn_question_response(H):
     cur.execute(f"UPDATE statement_table SET sentence='{H}' WHERE id={last_id}")
     db.commit()
     B = "Thank you! I have learnt this."
-    return B, LearnResponse.MESSAGE.name
+    return B, chatbot.LearnResponse.MESSAGE.name
 
 
 def clear_table(table_name):
