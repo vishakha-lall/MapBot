44a2844a3025d9325ae072b3766ca5adebef5567
rits1272 <ritikjain1272@gmail.com>
2020-04-08 23:01:21 +0530

    refactor code

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, request
 from chatbot import message_to_bot, setup
-from config import slack_client_id, slack_client_secret, slack_oauth_scope
-import os, certifi, ssl
+from config import slack_client_id, slack_client_secret
+from config import slack_oauth_scope
+import certifi
+import ssl
 import slack
 
 CLIENT_ID = slack_client_id
@@ -12,20 +14,29 @@ app = Flask(__name__)
 ssl_context = ssl.create_default_context(cafile=certifi.where())
 
 
-
 @app.route("/chatbot/<user_input>", methods=["GET"])
 def chat(user_input):
     try:
         response = message_to_bot(user_input, clf, learn_response)
     except Exception as e:
         print(e)
-        return jsonify({'message': ('Unable to get response', learn_response)}, 500)
+        return jsonify(
+            {'message': ('Unable to get response', learn_response)},
+            500)
 
     return jsonify({'message': response}, 200)
 
+
 @app.route("/begin_auth", methods=["GET"])
 def pre_install():
-    return f'''<a href="https://slack.com/oauth/authorize?scope={OAUTH_SCOPE}&client_id={CLIENT_ID}"><img alt=""Add to Slack"" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>'''
+    return f'''<a href="https://slack.com/oauth/authorize?
+            scope={OAUTH_SCOPE}&client_id={CLIENT_ID}">
+            <img alt=""Add to Slack"" height="40" width="139"
+            src="https://platform.slack-edge.com/img/add_to_slack.png"
+            srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x,
+            https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>
+            '''
+
 
 @app.route('/post_auth', methods=['GET', 'POST'])
 def post_install():
@@ -37,12 +48,12 @@ def post_install():
     # Retrieve the auth code from the request params
     auth_code = request.args['code']
     # An empty string is a valid token from this request
-    client = slack.WebClient(token="",ssl=ssl_context)
+    client = slack.WebClient(token="", ssl=ssl_context)
     # Request the auth token from slack
     response = client.oauth_access(
-            client_id=CLIENT_ID,
-            client_secret=CLIENT_SECRET,
-            code=auth_code
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        code=auth_code
     )
     BOT_TOKEN = response['bot']['bot_access_token']
     f = open('ENV/.env', 'a')
--- slackbot.py
+++ slackbot.py
@@ -1,6 +1,6 @@
-import os
 from slack import RTMClient
-import ssl, certifi
+import ssl
+import certifi
 import requests
 from config import slack_bot_token
 
@@ -9,40 +9,43 @@ URL = 'http://localhost:5000/chatbot/'
 
 ssl_context = ssl.create_default_context(cafile=certifi.where())
 
+
 @RTMClient.run_on(event="message")
 def mapbot(**payload):
-  '''
-  This function is a listener for 
-  message events happening in the 
-  the workspace.
-  '''
-  data = payload['data']
-  web_client = payload['web_client']
-  bot_id = data.get('bot_id', '')
+    '''
+    This function is a listener for
+    message events happening in the
+    the workspace.
+    '''
+    data = payload['data']
+    web_client = payload['web_client']
+    bot_id = data.get('bot_id', '')
 
-  # Ignore Bot Messages
-  if bot_id == '':
-    channel_id = data['channel']
-    # User message from slack
-    text = data.get('text', '')
-    text = text.split('>')[-1].strip()
+    # Ignore Bot Messages
+    if bot_id == '':
+        channel_id = data['channel']
+        # User message from slack
+        text = data.get('text', '')
+        text = text.split('>')[-1].strip()
 
-    response = ""
-    if 'help' in text.lower():
-      user = data.get('user', '')
-      response = f"Hi <@{user}>! I am Mapbot :)"
-    else:
-      chatbot_response = requests.get(f"{URL}{text}").json()
-      response = chatbot_response[0]['message'][0]
-    
+        response = ""
+        if 'help' in text.lower():
+            user = data.get('user', '')
+            response = f"Hi <@{user}>! I am Mapbot :)"
+        else:
+            chatbot_response = requests.get(f"{URL}{text}").json()
+            response = chatbot_response[0]['message'][0]
+
     # Sends the message to the slack
     web_client.chat_postMessage(
         channel=channel_id,
         text=response
       )
+
+
 try:
-  rtm_client = RTMClient(token=slack_bot_token, ssl=ssl_context)
-  print("Mapbot is up and running!")
-  rtm_client.start()
+    rtm_client = RTMClient(token=slack_bot_token, ssl=ssl_context)
+    print("Mapbot is up and running!")
+    rtm_client.start()
 except Exception as err:
-  print("Slack token or server error", err)
+    print("Slack token or server error", err)
