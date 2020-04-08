63c30374a36684e0a2c20ea45a0f85af2be4635b
Ankur Chattopadhyay <39518771+chttrjeankr@users.noreply.github.com>
2020-04-08 21:14:38 +0530

    Travis CI on test_databaseconnect.py doesn't work. #120 resolved (#121)
    * modified test_databaseconnect.py
    
    * test_googleMapsApiModule made to skip API functions for now.
    
    * introduced strict psf/black check
    
    * formatting for TravisCI

--- databaseconnect.py
+++ databaseconnect.py
@@ -11,19 +11,32 @@ def connection_to_database():
     import config
     import mysql.connector
     from time import sleep
+    import sys
+
+    called_from = sys.argv[0]
+    logging.debug(called_from)
 
     max_tries = 10
     tries = 1
     conn = None
     while tries <= max_tries:
         try:
-            conn = mysql.connector.connect(
-                user=config.user,
-                password=config.password,
-                host=config.host,
-                port=config.port,
-                database=config.database,
-            )
+            if called_from.endswith("pytest"):
+                conn = mysql.connector.connect(
+                    user="root",
+                    password="",
+                    host=config.host,
+                    port=config.port,
+                    database="pytest_testdb",
+                )
+            else:
+                conn = mysql.connector.connect(
+                    user=config.user,
+                    password=config.password,
+                    host=config.host,
+                    port=config.port,
+                    database=config.database,
+                )
             if conn.is_connected():
                 # logging.debug("Connected")
                 logging.debug("MySQL connected")
@@ -48,19 +61,21 @@ def setup_database():
     db = connection_to_database()
     cur = db.cursor()
     cur.execute(
-      "CREATE TABLE IF NOT EXISTS chat_table(id INTEGER PRIMARY KEY AUTO_INCREMENT, root_word VARCHAR(40), subject VARCHAR(40), verb VARCHAR(40), sentence VARCHAR(200))"  # noqa: E501
-    ) 
+        "CREATE TABLE IF NOT EXISTS chat_table(id INTEGER PRIMARY KEY AUTO_INCREMENT, root_word VARCHAR(40), subject VARCHAR(40), verb VARCHAR(40), sentence VARCHAR(200))"  # noqa: E501
+    )
     cur.execute(
-      "CREATE TABLE IF NOT EXISTS statement_table(id INTEGER PRIMARY KEY AUTO_INCREMENT, root_word VARCHAR(40), subject VARCHAR(40), verb VARCHAR(40), sentence VARCHAR(200))"  # noqa: E501
-    ) 
+        "CREATE TABLE IF NOT EXISTS statement_table(id INTEGER PRIMARY KEY AUTO_INCREMENT, root_word VARCHAR(40), subject VARCHAR(40), verb VARCHAR(40), sentence VARCHAR(200))"  # noqa: E501
+    )
     cur.execute(
-      "CREATE TABLE IF NOT EXISTS question_table(id INTEGER PRIMARY KEY AUTO_INCREMENT, root_word VARCHAR(40), subject VARCHAR(40), verb VARCHAR(40), sentence VARCHAR(200))"  # noqa: E501
-    ) 
+        "CREATE TABLE IF NOT EXISTS question_table(id INTEGER PRIMARY KEY AUTO_INCREMENT, root_word VARCHAR(40), subject VARCHAR(40), verb VARCHAR(40), sentence VARCHAR(200))"  # noqa: E501
+    )
     cur.execute(
-      "CREATE TABLE IF NOT EXISTS directions_table(id INTEGER PRIMARY KEY AUTO_INCREMENT, origin_location VARCHAR(100), destination_location VARCHAR(100))"  # noqa: E501
-    ) 
-    return db
-    
+        "CREATE TABLE IF NOT EXISTS directions_table(id INTEGER PRIMARY KEY AUTO_INCREMENT, origin_location VARCHAR(100), destination_location VARCHAR(100))"  # noqa: E501
+    )
+    cur.execute("SHOW TABLES")
+    TABLES = cur.fetchall()
+    return TABLES
+
 
 @logger_config.logger
 # add classified sentences to database
@@ -68,39 +83,46 @@ def add_to_database(classification, subject, root, verb, H):
     db = connection_to_database()
     cur = db.cursor()
     cur = db.cursor(prepared=True)
-    if classification == 'C':
-        cur.execute(
-            "INSERT INTO chat_table(root_word,verb,sentence) VALUES (%s, %s, %s)", (str(root), str(verb), H,)
-        )
-        db.commit()
-    elif classification == "Q":
-        cur.execute("SELECT sentence FROM question_table")
-        res = cur.fetchall()
-        exist = 0
-        for r in res:
-            if r[-1] == H:
-                exist = 1
-                break
-        if exist == 0:
-            # do not add if question already exists
+    try:
+        if classification == "C":
             cur.execute(
-              "INSERT INTO question_table(subject,root_word,verb,sentence) VALUES (%s,%s,%s,%s)", (str(subject), str(root),str(verb),H,)
+                "INSERT INTO chat_table(root_word,verb,sentence) VALUES (%s, %s, %s)",
+                (str(root), str(verb), H,),
             )
             db.commit()
+        elif classification == "Q":
+            cur.execute("SELECT sentence FROM question_table")
+            res = cur.fetchall()
+            exist = 0
+            for r in res:
+                if r[-1] == H:
+                    exist = 1
+                    break
+            if exist == 0:
+                # do not add if question already exists
+                cur.execute(
+                    "INSERT INTO question_table(subject,root_word,verb,sentence) VALUES (%s,%s,%s,%s)",
+                    (str(subject), str(root), str(verb), H,),
+                )
+                db.commit()
+        else:
+            cur.execute("SELECT sentence FROM statement_table")
+            res = cur.fetchall()
+            exist = 0
+            for r in res:
+                if r[-1] == H:
+                    exist = 1
+                    break
+            if exist == 0:  # do not add if question already exists
+                cur.execute(
+                    "INSERT INTO statement_table(subject,root_word,verb,sentence) VALUES (%s,%s,%s,%s)",
+                    (str(subject), str(root), str(verb), H,),
+                )
+                db.commit()
+    except Exception as e:
+        return e
     else:
-        cur.execute("SELECT sentence FROM statement_table")
-        res = cur.fetchall()
-        exist = 0
-        for r in res:
-            if r[-1] == H:
-                exist = 1
-                break
-        if exist == 0:    # do not add if question already exists
-            cur.execute(
-              "INSERT INTO statement_table(subject,root_word,verb,sentence) VALUES (%s,%s,%s,%s)", (str(subject), str(root), str(verb), H,)
-            )
-            db.commit()
-    return db
+        return "Success"
 
 
 @logger_config.logger
@@ -115,7 +137,7 @@ def get_chat_response():
     import random
 
     chat_id = random.randint(1, total_chat_records)
-    cur.execute("SELECT sentence FROM chat_table WHERE id = %s",(str(chat_id),))
+    cur.execute("SELECT sentence FROM chat_table WHERE id = %s", (str(chat_id),))
     res = cur.fetchone()
     B = res[0]
     return B
@@ -135,7 +157,7 @@ def get_question_response(subject, root, verb):
                 break
         if found == 1:
             cur.execute(
-                "SELECT sentence FROM statement_table WHERE verb= %s",(str(verb),)
+                "SELECT sentence FROM statement_table WHERE verb= %s", (str(verb),)
             )
             res = cur.fetchone()
             B = res[0]
@@ -156,11 +178,12 @@ def get_question_response(subject, root, verb):
                 "SELECT verb FROM statement_table WHERE subject= %s", (str(subject[0]),)
             )
             res = cur.fetchone()
-            checkVerb = res[0]   
+            checkVerb = res[0]
             # checkVerb is a string while verb is a list. checkVerb ['verb']
             if checkVerb == "[]":
                 cur.execute(
-                    "SELECT sentence FROM statement_table WHERE subject= %s", (str(subject[0]),)
+                    "SELECT sentence FROM statement_table WHERE subject= %s",
+                    (str(subject[0]),),
                 )
                 res = cur.fetchone()
                 B = res[0]
@@ -168,7 +191,8 @@ def get_question_response(subject, root, verb):
             else:
                 if checkVerb[2:-2] == verb[0]:
                     cur.execute(
-                        "SELECT sentence FROM statement_table WHERE subject= %s", (str(subject[0]),)
+                        "SELECT sentence FROM statement_table WHERE subject= %s",
+                        (str(subject[0]),),
                     )
                     res = cur.fetchone()
                     B = res[0]
@@ -181,15 +205,18 @@ def get_question_response(subject, root, verb):
             return B, chatbot.LearnResponse.TRAIN_ME.name
 
 
+# May be redundant. Can be handled by add_to_database("O", subject, root, verb, H)
 @logger_config.logger
 def add_learnt_statement_to_database(subject, root, verb):
     db = connection_to_database()
     cur = db.cursor()
     cur.execute(
-      "INSERT INTO statement_table(subject,root_word,verb) VALUES (%s,%s,%s)", (str(subject), str(root), str(verb),)
+        "INSERT INTO statement_table(subject,root_word,verb) VALUES (%s,%s,%s)",
+        (str(subject), str(root), str(verb),),
     )
     db.commit()
-    return db
+    # return db
+    return True
 
 
 @logger_config.logger
@@ -199,7 +226,9 @@ def learn_question_response(H):
     cur.execute("SELECT id FROM statement_table ORDER BY id DESC")
     res = cur.fetchone()
     last_id = res[0]
-    cur.execute("UPDATE statement_table SET sentence = %s WHERE id = %s",(H, str(last_id),))
+    cur.execute(
+        "UPDATE statement_table SET sentence = %s WHERE id = %s", (H, str(last_id),)
+    )
     db.commit()
     B = "Thank you! I have learnt this."
     return B, chatbot.LearnResponse.MESSAGE.name
@@ -215,10 +244,10 @@ def clear_table(table_name):
         logging.debug("The following tables will be cleaned:\n")
         for table in tables_to_be_cleaned:
             describe_table(cur, table)
-        
+
         if input("Enter 'Y' to confirm cleaning of BOTH tables: ") in ("Y", "y",):
             for table in tables_to_be_cleaned:
-                cur.execute("DELETE FROM %s",(table,))  
+                cur.execute("DELETE FROM %s", (table,))
             db.commit()
             logging.debug("Tables cleaned successfully")
         else:
@@ -230,7 +259,7 @@ def clear_table(table_name):
 
         if input("Enter 'Y' to confirm: ") in ("Y", "y"):
             logging.debug("Table cleaned successfully")
-            cur.execute("DELETE FROM %s",(table_name,))
+            cur.execute("DELETE FROM %s", (table_name,))
             db.commit()
         else:
             logging.debug("Table cleaning skipped.")
@@ -240,11 +269,11 @@ def clear_table(table_name):
 
 @logger_config.logger
 def describe_table(cur, table_name):
-    cur.execute("DESC %s",(table_name,))
+    cur.execute("DESC %s", (table_name,))
     res = cur.fetchall()
     column_names = [col[0] for col in res]
-    
-    cur.execute("SELECT COUNT(*) FROM %s",(table_name,))
+
+    cur.execute("SELECT COUNT(*) FROM %s", (table_name,))
     res = cur.fetchall()
     records_no = res[0][0]
 
@@ -252,6 +281,5 @@ def describe_table(cur, table_name):
     logging.debug("Columns:", column_names)
     logging.debug("Number of existing records:", records_no)
     logging.debug()
-    
+
     return records_no
- 
--- test_databaseconnect.py
+++ test_databaseconnect.py
@@ -1,86 +1,28 @@
 import databaseconnect
-import config
-import pytest
-import mysql.connector as mysql
 
 
 class TestClass:
-    @pytest.fixture(scope="session", autouse=True)
-    def setup_init(self):
-        # Will be executed before the first test
-        main_database = config.database
-        config.database = "test"
-        try:
-            test_db = mysql.connect(
-                host=config.host,
-                user=config.user,
-                passwd=config.password,
-                database=config.database,
-            )
-
-            cursor = test_db.cursor()
-            cursor.execute("CREATE DATABASE {}".format(config.database))
-            print("test database created")
-
-        except Exception:
-            print("Failed to create test database")
-            # rolling back to main db
-            config.database = main_database
-            pytest.exit("Exiting test!")
-
-        yield test_db
-
-        # Will be executed after the last test is executed
-        try:
-            mycursor = test_db.cursor()
-            mycursor.execute("DROP DATABASE {}".format(config.database))
-            mycursor.close()
-            print("test database deleted.")
-
-        except Exception:
-            print("Failed to delete test database.")
-
-        config.database = main_database
-
     def test_setup_database(self):
-        db = databaseconnect.setup_database()
-        cursor = db.cursor()
-        cursor.execute("SHOW TABLES")
-        tables = cursor.fetchall()
+        tables = databaseconnect.setup_database()
         expected_tables = [
-            ("chat_table"),
-            ("statement_table"),
-            ("question_table"),
-            ("directions_table"),
+            ("chat_table",),
+            ("directions_table",),
+            ("question_table",),
+            ("statement_table",),
         ]
-        assert tables.sort() == expected_tables.sort()
+        assert sorted(tables) == sorted(expected_tables)
 
     def test_add_to_database_of_chat_table(self):
-        db = databaseconnect.add_to_database("C", "subject", "root", "verb", "H")
-        cursor = db.cursor()
-        cursor.execute(
-            "select * from chat_table where root_word='root' and verb='verb' and sentence='H'"
-        )
-        res = cursor.fetchone()[0]
-        assert res == 1
+        res = databaseconnect.add_to_database("C", "subject", "root", "verb", "H")
+        assert res == "Success"
 
     def test_add_to_database_of_question_table(self):
-        db = databaseconnect.add_to_database("Q", "subject", "root", "verb", "H")
-        cursor = db.cursor()
-        cursor.execute(
-            "select * from question_table where subject='subject' and root_word='root' and verb='verb' and sentence='H'"
-        )
-        res = cursor.fetchone()[0]
-        assert res == 1
+        res = databaseconnect.add_to_database("Q", "subject", "root", "verb", "H")
+        assert res == "Success"
 
     def test_add_to_database_of_statement_table(self):
-        db = databaseconnect.add_to_database("O", "subject", "root", "verb", "H")
-        cursor = db.cursor()
-        cursor.execute(
-            "select * from statement_table where subject='subject' and root_word='root' and verb='verb' and sentence='H'"
-        )
-        res = cursor.fetchone()[0]
-        assert res == 1
+        res = databaseconnect.add_to_database("O", "subject", "root", "verb", "H")
+        assert res == "Success"
 
     def test_get_chat_response(self):
         response = databaseconnect.get_chat_response()
@@ -95,44 +37,13 @@ class TestClass:
         assert type(response) is tuple
 
     def test_add_learnt_statement_to_database(self):
-        db = databaseconnect.add_learnt_statement_to_database("subject", "root", "verb")
-        cursor = db.cursor()
-        cursor.execute(
-            "select * from question_table where subject='subject' and root_word='root' and verb='verb'"
+        res = databaseconnect.add_learnt_statement_to_database(
+            "subject", "root", "verb"
         )
-        res = cursor.fetchone()[0]
-        assert res == 1
+        # test not needed
+        # handled by test_add_to_database_of_statement_table()
+        assert res
 
     def test_learn_question_response(self):
         response = databaseconnect.learn_question_response("H")
         assert type(response) is tuple
-
-    def test_clear_table_with_chat_table(self, monkeypatch):
-        from io import StringIO
-
-        yes = StringIO("y\n")
-        monkeypatch.setattr("sys.stdin", yes)
-
-        db = databaseconnect.clear_table("chat_table")
-
-        cursor = db.cursor()
-        cursor.execute("select * from chat_table")
-        entries = cursor.fetchone()
-        assert entries is None
-
-    def test_clear_table_with_statement_or_question_table(self, monkeypatch):
-        from io import StringIO
-
-        yes = StringIO("y\n")
-        monkeypatch.setattr("sys.stdin", yes)
-
-        db = databaseconnect.clear_table("statement_table")
-
-        cursor = db.cursor()
-        cursor.execute("select * from statement_table")
-        entries_1 = cursor.fetchone()
-
-        cursor.execute("select * from question_table")
-        entries_2 = cursor.fetchone()
-
-        assert entries_1 is None and entries_2 is None
