1e32acddcf45b247c3c249397bc4bb36e39396e3
rits1272 <ritikjain1272@gmail.com>
2020-04-08 23:12:43 +0530

    resolve travis-ci issue

--- app.py
+++ app.py
@@ -20,48 +20,44 @@ def chat(user_input):
         response = message_to_bot(user_input, clf, learn_response)
     except Exception as e:
         print(e)
-        return jsonify(
-            {'message': ('Unable to get response', learn_response)},
-            500)
+        return jsonify({"message": ("Unable to get response", learn_response)}, 500)
 
-    return jsonify({'message': response}, 200)
+    return jsonify({"message": response}, 200)
 
 
 @app.route("/begin_auth", methods=["GET"])
 def pre_install():
-    return f'''<a href="https://slack.com/oauth/authorize?
+    return f"""<a href="https://slack.com/oauth/authorize?
             scope={OAUTH_SCOPE}&client_id={CLIENT_ID}">
             <img alt=""Add to Slack"" height="40" width="139"
             src="https://platform.slack-edge.com/img/add_to_slack.png"
             srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x,
             https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>
-            '''
+            """
 
 
-@app.route('/post_auth', methods=['GET', 'POST'])
+@app.route("/post_auth", methods=["GET", "POST"])
 def post_install():
-    '''
+    """
         After user authorizes the bot to access its workspace slack
         generates a temporary code which this function exchanges with
         the BOT_USER_ACCESS_TOKEN
-    '''
+    """
     # Retrieve the auth code from the request params
-    auth_code = request.args['code']
+    auth_code = request.args["code"]
     # An empty string is a valid token from this request
     client = slack.WebClient(token="", ssl=ssl_context)
     # Request the auth token from slack
     response = client.oauth_access(
-        client_id=CLIENT_ID,
-        client_secret=CLIENT_SECRET,
-        code=auth_code
+        client_id=CLIENT_ID, client_secret=CLIENT_SECRET, code=auth_code
     )
-    BOT_TOKEN = response['bot']['bot_access_token']
-    f = open('ENV/.env', 'a')
+    BOT_TOKEN = response["bot"]["bot_access_token"]
+    f = open("ENV/.env", "a")
     f.write(f"\nSLACK_BOT_TOKEN={BOT_TOKEN}")
     f.close()
     return "<h2 align='center'>Auth Completed! You may close this window</h2>"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     clf, learn_response = setup()
     app.run(debug=True)
--- slackbot.py
+++ slackbot.py
@@ -5,42 +5,39 @@ import requests
 from config import slack_bot_token
 
 # API ENDPOINT
-URL = 'http://localhost:5000/chatbot/'
+URL = "http://localhost:5000/chatbot/"
 
 ssl_context = ssl.create_default_context(cafile=certifi.where())
 
 
 @RTMClient.run_on(event="message")
 def mapbot(**payload):
-    '''
+    """
     This function is a listener for
     message events happening in the
     the workspace.
-    '''
-    data = payload['data']
-    web_client = payload['web_client']
-    bot_id = data.get('bot_id', '')
+    """
+    data = payload["data"]
+    web_client = payload["web_client"]
+    bot_id = data.get("bot_id", "")
 
     # Ignore Bot Messages
-    if bot_id == '':
-        channel_id = data['channel']
+    if bot_id == "":
+        channel_id = data["channel"]
         # User message from slack
-        text = data.get('text', '')
-        text = text.split('>')[-1].strip()
+        text = data.get("text", "")
+        text = text.split(">")[-1].strip()
 
         response = ""
-        if 'help' in text.lower():
-            user = data.get('user', '')
+        if "help" in text.lower():
+            user = data.get("user", "")
             response = f"Hi <@{user}>! I am Mapbot :)"
         else:
             chatbot_response = requests.get(f"{URL}{text}").json()
-            response = chatbot_response[0]['message'][0]
+            response = chatbot_response[0]["message"][0]
 
     # Sends the message to the slack
-    web_client.chat_postMessage(
-        channel=channel_id,
-        text=response
-      )
+    web_client.chat_postMessage(channel=channel_id, text=response)
 
 
 try:
