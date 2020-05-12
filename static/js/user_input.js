897ce58d48dcda190b192aaae51714223a263ec4
Ankur Chattopadhyay <chttrjeankr@gmail.com>
2020-05-13 01:50:48 +0530

    restructured JS

---
+++ static/js/user_input.js
@@ -0,0 +1,50 @@
+function add_user_data(innerText) {
+    var rownode = document.createElement('div');
+    rownode.setAttribute('class', 'row justify-content-md-center');
+    var usernode = document.createElement("div");
+    usernode.setAttribute('class', 'col shadow-sm p-3 mb-1 text-left text-break');
+    usernode.innerText = innerText;
+    rownode.appendChild(usernode);
+    return rownode;
+}
+
+function add_bot_data(botText, rownode) {
+    var botnode = document.createElement("div");
+    botnode.setAttribute('class', 'col shadow-sm p-3 mb-1 text-right text-break');
+    botnode.innerText = botText;
+    rownode.appendChild(botnode);
+    document.getElementById("chats").appendChild(rownode);
+}
+
+$(function() {
+    $("#form_usrInput").on('submit', function(event) {
+        var that = this;
+        var usrText = $('#usrInput').val();
+        var rownode = add_user_data(usrText);
+        $.ajax({
+                data: {
+                    usrInput: usrText,
+                },
+                type: 'POST',
+                url: '/process',
+                beforeSend: function() {
+                    $("#loading-spinner").show();
+                },
+                success: function(data) {
+                    $("#loading-spinner").hide();
+                    that.reset();
+                }
+            })
+            .done(function(data) {
+                add_bot_data(data.botText, rownode);
+            });
+        document
+            .getElementById("form_usrInput")
+            .scrollIntoView({
+                block: "end",
+                behavior: "smooth",
+                inline: "nearest"
+            });
+        event.preventDefault();
+    });
+});
