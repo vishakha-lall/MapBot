4a8eeb822959dc575ce6408ae32fd77a02302303
Ankur Chattopadhyay <chttrjeankr@gmail.com>
2020-05-18 00:05:43 +0530

    chore: remove start.sh
    start.sh isn't needed anymore for the new 
    deployment using Docker approach

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [ ! -d MapBot ]; then
-    git clone https://github.com/vishakha-lall/MapBot.git -b gssoc-master
-fi
-
-export GCLOUD_API_KEY=<YOUR_API_KEY_HERE>
-
-docker-compose -f ./MapBot/docker-compose.yml up -d && docker attach mapbot_bot
