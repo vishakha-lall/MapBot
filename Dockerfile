cb0ad02d55f3d21b628cde21d6d6699d0c3dd180
Ankur Chattopadhyay <39518771+chttrjeankr@users.noreply.github.com>
2020-05-23 17:29:46 +0530

    Docker deployment based on spaCy #171 resolved (#174)
    * refactor: remove java container and env variables
    
    * feat: remove CoreNLP dependency
    
    * Skip Travis CI
    
    [skip travis]

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,5 @@
 FROM python:3.6-slim-stretch
 
-RUN apt-get update && apt-get install -y --no-install-recommends \
-    wget=1.18-5+deb9u3 \
-    unzip=6.0-21+deb9u2 \
-    && apt-get clean \
-    && rm -rf /var/lib/apt/lists/*
-
 COPY requirements.txt ./requirements.txt
 RUN pip install -r requirements.txt
 
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,14 +2,5 @@
 
 set -ex
 
-if [ ! -d ./stanford-corenlp-full-2018-10-05 ]; then
-    wget http://nlp.stanford.edu/software/stanford-corenlp-full-2018-10-05.zip -nc -c
-fi
-
-if [ -f stanford-corenlp-full-2018-10-05.zip ]; then
-    unzip -n stanford-corenlp-full-2018-10-05.zip
-    rm stanford-corenlp-full-2018-10-05.zip
-fi
-
 export FLASK_APP=webapp.py
 flask run --host=0.0.0.0
