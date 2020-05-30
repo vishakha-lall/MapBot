a2bf2348fbcf4cd9d2b694a0f800f5123bd92adc
Ankur Chattopadhyay <39518771+chttrjeankr@users.noreply.github.com>
2020-05-30 13:47:16 +0530

    databaseconnect.describe_table() not working #196 resolved (#197)
    * fix: update SQL queries
    
    PostgreSQL alternative for DESC table was added
    
    f-strings implemented
    
    describe_table(...) is now _describe_table(...)
    
    raise Exception at wrong table name being passed
    
    * fix: missing option added

--- cleardb.py
+++ cleardb.py
@@ -5,9 +5,10 @@ print("          CLEARING TABLES")
 print("---------------------------------------")
 print("             Options Menu")
 print("1. ALL TABLES")
-print("2. chat_table")
-print("3. question_table and statement_table")
-print("Enter the respective option (1 or 2 or 3) according to the above menu:")
+print("2. `chat_table`")
+print("3. `directions_table`")
+print("4. `question_table` and `statement_table`")
+print("Enter the respective option (1 or 2 or 3 or 4) according to the above menu:")
 
 option = int(input())
 
@@ -15,9 +16,13 @@ if option == 1:
     clear_table("chat_table")
     clear_table("directions_table")
     clear_table("question_table")
+    # automatically deletes `statement_table` with it
 elif option == 2:
     clear_table("chat_table")
 elif option == 3:
+    clear_table("directions_table")
+elif option == 4:
     clear_table("question_table")
+    # automatically deletes `statement_table` with it
 else:
     print("Invalid option")
